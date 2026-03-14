5d638f85428e65e29ba7d01a5725cb4bb1283810
Vikram <vikram.lekhrajani@gmail.com>
2026-03-14 22:18:13 +0530

    Ground Work

---
+++ src/components/Contact.tsx
@@ -0,0 +1,69 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="mailto:vikram.lekhrajani@gmail.com" data-cursor="disable">
+                vikram.lekhrajani@gmail.com
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>BSc in Computer Science</p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/vikramlekhrajani/Portfolio#"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/vikram-lekharajani-6282a2251/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            <a
+              href="https://x.com/raxx21_official"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/vikram-lekharajani-6282a2251/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Instagram <MdArrowOutward />
+            </a>
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Vikram Lekharajani</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2025
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
